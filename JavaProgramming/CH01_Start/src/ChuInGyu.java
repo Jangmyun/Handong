3746b2e1948d60769426f7e0d9b856c11ded7a4f
Jangmyun <cndlsrb2739@gmail.com>
2024-03-22 12:42:59 +0900

    C01 과제

---
+++ JavaProgramming/CH01_Start/src/ChuInGyu.java
@@ -0,0 +1,9 @@
+public class ChuInGyu {
+  public static void main(String[] args) {
+    // 이름 : 추인규 (ChuInGyu)
+    // 학번 : 22000771
+    System.out.println("Name : ChuInGyu");
+    System.out.println("University : Handong Global University");
+    System.out.println("Student Number 22000771");
+  }
+}
