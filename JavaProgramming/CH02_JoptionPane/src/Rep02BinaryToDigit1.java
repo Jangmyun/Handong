316c8577ec277683bbe047f04a24187abe7bf2d8
Jangmyun <cndlsrb2739@gmail.com>
2024-03-31 00:51:27 +0900

    create Rep02BinaryToDigit1.java

---
+++ JavaProgramming/CH02_JoptionPane/src/Rep02BinaryToDigit1.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Rep02BinaryToDigit1 {
+  public static void main(String[] args) {
+    String inputBinary = JOptionPane.showInputDialog("Enter a 4-bit binary number:");
+    int binary = Integer.parseInt(inputBinary);
+
+    System.out.println(binary);
+    
+    
+  }
+}
