f670f5b3aef0b9173f1c087ca9e8f2ecb3cb2dca
Jangmyun <cndlsrb2739@gmail.com>
2024-04-18 12:04:25 +0900

    exercise study

--- DataStructure/study/11Queues.cpp
+++ DataStructure/study/11Queues.cpp
@@ -2,69 +2,54 @@
 using namespace std;
 #define Q_SIZE 100
 
-class queue
-{
+class queue {
   int q[Q_SIZE];
   int front, rear;
 
-public:
+ public:
   queue();
   void insert_q(int x);
   int delete_q();
   bool queue_full();
   bool queue_empty();
 };
-queue::queue()
-{
+queue::queue() {
   front = 0;
   rear = 0;
 }
-void queue::insert_q(int x)
-{
-  if (!queue_full())
-  {
+void queue::insert_q(int x) {
+  if (!queue_full()) {
     q[rear] = x;
     rear = (rear + 1) % Q_SIZE;
   }
 }
 
-int queue::delete_q()
-{
+int queue::delete_q() {
   int tmp;
-  if (!queue_empty())
-  {
+  if (!queue_empty()) {
     tmp = q[front];
     front = (front + 1) % Q_SIZE;
     return (tmp);
   }
 }
 
-bool queue::queue_full()
-{
-  if ((rear + 1) % Q_SIZE == front)
-  {
+bool queue::queue_full() {
+  if ((rear + 1) % Q_SIZE == front) {
     return true;
-  }
-  else
-  {
+  } else {
     return false;
   }
 }
 
-bool queue::queue_empty()
-{
-  if (rear == front)
-  {
+bool queue::queue_empty() {
+  if (rear == front) {
     return true;
-  }
-  else
-  {
+  } else {
     return false;
   }
 }
 
-int main()
-{
+int main() {
   // Queue 연산 > Create Queue , Insert, Delete, Full test, Empty test
   return 0;
 }
