5c4b957101703a82345e0993d01e4bdb3c603b26
Jangmyun <cndlsrb2739@gmail.com>
2024-05-24 19:20:20 +0900

    binaryTree 구현 및 주석

--- DataStructure/binaryTree/1traversalAlgorithm.cpp
+++ DataStructure/binaryTree/1traversalAlgorithm.cpp
@@ -9,6 +9,39 @@ class node {
  public:
   string getName() { return this->name; }
   double getScore() { return this->score; }
+  node *getLeft() { return this->left; }
+  node *getRight() { return this->right; }
 };
+
+// 주어진 p를 root로 하는 binary tree 를 inorder traverse
+void inorderTraverse(node *p) {
+  if (p == NULL) {
+    return;
+  }
+
+  inorderTraverse(p->getLeft());
+  cout << p->getName() << " : " << p->getScore() << endl;
+  inorderTraverse(p->getRight());
+}
+
+void preorderTraverse(node *p) {
+  if (p == NULL) {
+    return;
+  }
+
+  cout << p->getName() << " : " << p->getScore() << endl;
+  preorderTraverse(p->getLeft());
+  preorderTraverse(p->getRight());
+}
+
+void postorderTraverse(node *p) {
+  if (p == NULL) {
+    return;
+  }
+
+  postorderTraverse(p->getLeft());
+  postorderTraverse(p->getRight());
+  cout << p->getName() << " : " << p->getScore() << endl;
+}
 
 int main() { return 0; }
---
+++ DataStructure/binaryTree/binaryTree1.cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+using namespace std;
+
+class Node {
+ public:
+  string name;
+  double score;
+  Node *left, *right;
+  void set_date(string s, double n) {
+    name = s;
+    score = n;
+  }
+};
+
+// 성공시 1, 해당 Node없으면 0 , Node는 있는데 left가 NULL이 아닌 경우 -1 리턴
+int node_insert_left(Node *p, string tName, Node tNode) {
+  if (p == NULL) {  // root가 NULL이면 0리턴
+    return 0;
+  }
+  if (p->name == tName) {  // tName과 일치하는 노드를 찾았을 때
+    if (p->left !=
+        NULL) {  // tName과 일치하는 노드의 left Node 가 이미 있으면 -1 리턴
+      return -1;
+    }
+    Node *t;
+    t = new Node;
+    *t = tNode;
+    t->left = NULL;
+    t->right = NULL;
+    p->left = t;
+    return 1;
+  } else {
+    int result = node_insert_left(p->left, tName, tNode);
+    if (result != 0) {
+      return result;
+    }
+    return node_insert_left(p->right, tName, tNode);
+  }
+}
+
+int node_insert_right(Node *p, string tName, Node tNode) {
+  if (p == NULL) {
+    return 0;
+  }
+  if (p->name == tName) {
+    if (p->right != NULL) {
+      return -1;
+    }
+    Node *t;
+    t = new Node;
+    t->left = NULL;
+    t->right = NULL;
+    p->right = t;
+    return 1;
+  } else {
+    int result = node_insert_right(p->left, tName, tNode);
+    if (result != 0) {
+      return result;
+    }
+    return node_insert_right(p->right, tName, tNode);
+  }
+}
+
+double sum_allNodes(Node *p) {
+  if (p == NULL) {
+    return 0;
+  }
+  return sum_allNodes(p->left) + sum_allNodes(p->right) + p->score;
+}
+
+void inorder_print(Node *p) {
+  if (p == NULL) {
+    return;
+  }
+
+  inorder_print(p->left);
+  cout << p->name << " : " << p->score << endl;
+  inorder_print(p->right);
+}
+
+class MyTree {
+ public:
+  int nodeCount;
+  Node *root;
+  MyTree() {
+    nodeCount = 0;
+    root = NULL;
+  }
+  int insert_root(Node t) {  // 실패시 0 성공시 1 리턴
+    // root가 NULL이 아니면 실패 (이미 Root에 뭔가 있음)
+    if (root != NULL) {
+      return 0;
+    }
+    // Node 공간 생성
+    Node *p;
+    p = new Node;
+    *p = t;
+    p->left = NULL;
+    p->right = NULL;
+    // 생성한 Node를 root로
+    root = p;
+    nodeCount++;
+    return 1;
+  }
+
+  // name이 tname인 node를 찾고 그 node 왼쪽에 t추가
+  int insert_left(string tName, Node t) {
+    int result;
+
+    result = node_insert_left(root, tName, t);
+    if (result == 1) {
+      nodeCount++;
+    }
+    return result;
+  }
+  int insert_right(string tName, Node t) {
+    int result;
+    result = node_insert_right(root, tName, t);
+    if (result == 1) {
+      nodeCount++;
+    }
+    return result;
+  }
+
+  double score_sum() { return sum_allNodes(root); }
+  double score_average() { return score_sum() / nodeCount; };
+  void printData() { inorder_print(root); }
+
+  // non recursive inorder traversal ( stack 사용 )
+  void nonRecursiveInorder() {
+    stack<Node *> s;
+    Node *t;
+    t = root;
+    // root 부터 반복
+    while (1) {
+      // 현재 node부터 왼쪽 끝 노드 찾을 때까지 node를 스택에 넣기
+      while (t != NULL) {
+        s.push(t);
+        t = t->left;
+      }
+      // stack이 empty면 리턴
+      while (t == NULL) {
+        if (s.empty()) {
+          return;
+        }
+        // stack에서 한개 원소 pop
+        t = s.top();
+        s.pop();
+      }
+      cout << t->name << " : " << t->score << endl;
+      t = t->right;
+    }
+  }
+
+  void levelOrder() {
+    queue<Node *> q;
+    Node *t;
+
+    if (root == NULL) {
+      return;
+    }
+    q.push(root);
+    while (1) {
+      if (q.empty()) {
+        return;
+      }
+      t = q.front();
+      q.pop();
+      cout << t->name << " : " << t->score << endl;
+      if (t->left != NULL) {
+        q.push(t->left);
+      }
+      if (t->right != NULL) {
+        q.push(t->right);
+      }
+    }
+  }
+};
+
+int main() { return 0; }
